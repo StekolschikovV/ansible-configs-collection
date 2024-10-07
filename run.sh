a1d913ebec14260bf877dc1c2825465616ef66de
mk <v.a.stekolschikov@gmail.com>
2024-10-07 20:14:15 +0100

    added grafana

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 ansible-playbook -i hosts ubuntu_setup.yml
-ansible-playbook -i hosts grafana_loki_setup.yml
+
+ansible-playbook -i hosts ./func/loki_setup.yml
+ansible-playbook -i hosts ./func/grafana_setup.yml
