083ea43431edd9a839a69ebb582af1487f416499
mk <v.a.stekolschikov@gmail.com>
2024-10-07 20:32:56 +0100

    tmp

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
-ansible-playbook -i hosts ubuntu_setup.yml
+ansible-playbook -i grafana_loki_hosts ubuntu_setup.yml
 
-ansible-playbook -i hosts ./func/loki_setup.yml
-ansible-playbook -i hosts ./func/grafana_setup.yml
+ansible-playbook -i grafana_loki_hosts ./func/loki_setup.yml
+ansible-playbook -i grafana_loki_hosts ./func/grafana_setup.yml
+
+ansible-playbook -i promtail_hosts promtail_setup.yml
+
+
+
